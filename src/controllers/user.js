9a4db3df34a51b6fc352ee7db2545360dd343520
Eduardo Santiago <eduardosantiago_@hotmail.com>
2023-10-23 20:24:30 -0300

    add: Yup

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,39 @@
 import { User } from "../models";
+import * as Yup from "yup";
+import bcrypt from "bcrypt";
 
 class UserController {
   async create(req, res) {
-    const user = new User({
-      name: "Eduardo",
-      email: "eduardo@teste.com",
-      password: "teste123",
-      password_hash: "teste123",
-      reset_password_token: "teste",
-      reset_password_token_sent_at: new Date(),
-      avatar_url: "teste_url",
-    });
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .required("Nome é obrigatório")
+          .min(3, "Nome deve conter ao menos 3 caracteres"),
+        email: Yup.string()
+          .email("E-mail inválido")
+          .required("E-mail é obrigatório"),
+        password: Yup.string()
+          .required("Senha é obrigatória")
+          .min(6, "Senha deve conter ao menos 6 caracteres"),
+      });
 
-    await user.save();
+      await schema.validade(req.body);
 
-    return res.json({ user });
+      const hashPassword = await bcrypt.hash(req.body.password, 8);
+
+      const user = new User({
+        ...req.body,
+        password: "",
+        password_hash: hashPassword,
+      });
+
+      await user.save();
+
+      return res.json({ user });
+    } catch (error) {
+      console.log({ error });
+      return res.status(400).json({ error: error?.message });
+    }
   }
 }
 
