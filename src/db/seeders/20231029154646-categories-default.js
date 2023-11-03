d746edc6bd804b93f128d4193f9b1751655c909c
Eduardo Santiago <eduardosantiago_@hotmail.com>
2023-11-03 19:33:41 -0300

    feat: add userbook relation, seeds and search controller

--- src/db/seeders/20231029154646-categories-default.js
+++ src/db/seeders/20231029154646-categories-default.js
@@ -1,82 +1,97 @@
 "use strict";
 
+const { query } = require("express");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Categories", [
       {
+        id: 1,
         name: "Ficção",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 2,
         name: "Romance",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 3,
         name: "Terror",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 4,
         name: "Autoajuda",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 5,
         name: "Negócios",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 6,
         name: "Biografia",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 7,
         name: "Mangá",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 8,
         name: "HQ",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 9,
         name: "Saúde",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 10,
         name: "Informática",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 11,
         name: "Suspense",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 12,
         name: "Fantasia",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 13,
         name: "Culinária",
         highlighted: false,
         createdAt: new Date(),
@@ -86,11 +101,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete("Categories", null, {});
   },
 };
